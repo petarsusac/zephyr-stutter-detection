2d21af9ce05c4c1de25e3bc661cb4f3b61deada0
Petar Susac <petar.susac@fer.hr>
2024-02-09 18:52:39 +0100

    DMIC example

--- app/src/main.c
+++ app/src/main.c
@@ -7,21 +7,110 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
 
+#include <zephyr/audio/dmic.h>
+
 #include <zephyr/logging/log.h>
+
+#define MAX_SAMPLE_RATE 16000
+#define SAMPLE_BIT_WIDTH 16
+#define BYTES_PER_SAMPLE sizeof(int16_t)
+
+#define READ_TIMEOUT_MS 1000
+
+#define BLOCK_SIZE(_sample_rate, _number_of_channels) \
+	(BYTES_PER_SAMPLE * (_sample_rate / 10) * _number_of_channels)
+
+#define MAX_BLOCK_SIZE   BLOCK_SIZE(MAX_SAMPLE_RATE, 2)
+#define BLOCK_COUNT      4
+K_MEM_SLAB_DEFINE_STATIC(mem_slab, MAX_BLOCK_SIZE, BLOCK_COUNT, 4);
+
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
 static struct gpio_dt_spec led_dev = GPIO_DT_SPEC_GET(DT_ALIAS(led2), gpios);
+static const struct device *const dmic_dev = DEVICE_DT_GET(DT_NODELABEL(mp34dt06j));
+
+static struct pcm_stream_cfg stream = {
+	.pcm_width = SAMPLE_BIT_WIDTH,
+	.mem_slab  = &mem_slab,
+	.pcm_rate = MAX_SAMPLE_RATE,
+	.block_size = BLOCK_SIZE(MAX_SAMPLE_RATE, 1),
+};
+
+static struct dmic_cfg cfg = {
+	.io = {
+		/* These fields can be used to limit the PDM clock
+			* configurations that the driver is allowed to use
+			* to those supported by the microphone.
+			*/
+		.min_pdm_clk_freq = 1800000,
+		.max_pdm_clk_freq = 2500000,
+	},
+	.streams = &stream,
+	.channel = {
+		.req_num_streams = 1,
+		.req_num_chan = 1,
+	},
+};
+
+
 
 int main(void)
 {
+	int ret;
 
 	gpio_pin_configure_dt(&led_dev, GPIO_OUTPUT);
 
-	gpio_pin_set_dt(&led_dev, 0);
+	gpio_pin_set_dt(&led_dev, 1);
+
+	LOG_INF("DMIC example");
+
+	if (!device_is_ready(dmic_dev)) {
+		LOG_ERR("%s is not ready", dmic_dev->name);
+		return 0;
+	}
+
+	ret = dmic_configure(dmic_dev, &cfg);
+
+	if (ret < 0)
+	{
+		LOG_ERR("DMIC configure error %d", ret);
+		return 0;
+	}
 
 	for (;;)
 	{
-		gpio_pin_toggle_dt(&led_dev);
+		ret = dmic_trigger(dmic_dev, DMIC_TRIGGER_START);
+
+		if (ret < 0)
+		{
+			LOG_ERR("DMIC trigger error %d", ret);
+			return 0;
+		}
+
+		for (int i = 0; i < 2*BLOCK_COUNT; i++)
+		{
+			void *buffer;
+			uint32_t size;
+
+			ret = dmic_read(dmic_dev, 0, &buffer, &size, READ_TIMEOUT_MS);
+
+			if (ret < 0)
+			{
+				LOG_ERR("%d - read failed with err %d", i, ret);
+				return 0;
+			}
+
+			LOG_INF("%d - got buffer %p of %u bytes", i, buffer, size);
+
+			k_mem_slab_free(&mem_slab, buffer);
+		}
+
+		ret = dmic_trigger(dmic_dev, DMIC_TRIGGER_STOP);
+		if (ret < 0) {
+			LOG_ERR("STOP trigger failed: %d", ret);
+			return 0;
+		}
+
 		k_msleep(1000);
 	}
 
