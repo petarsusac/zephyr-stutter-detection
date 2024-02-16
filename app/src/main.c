04dbe914294a4465760cc4900b48d73449019953
Petar Susac <petar.susac@fer.hr>
2024-02-16 19:37:36 +0100

    Buffered audio acquistion

--- app/src/main.c
+++ app/src/main.c
@@ -18,15 +18,19 @@
 #define READ_TIMEOUT_MS 1000
 
 #define BLOCK_SIZE(_sample_rate, _number_of_channels) \
-	(BYTES_PER_SAMPLE * (_sample_rate / 10) * _number_of_channels)
+	(BYTES_PER_SAMPLE * (_sample_rate / 100) * _number_of_channels)
 
 #define SINGLE_BLOCK_SIZE   BLOCK_SIZE(SAMPLE_RATE, 1)
 #define BLOCK_COUNT      4
+
+#define AUDIO_BUF_SIZE_MS 100
+#define AUDIO_BUF_SIZE ((SAMPLE_RATE * AUDIO_BUF_SIZE_MS) / MSEC_PER_SEC)
+
+LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+
 K_MEM_SLAB_DEFINE_STATIC(mem_slab, SINGLE_BLOCK_SIZE, BLOCK_COUNT, 4);
 
-LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
-
-static struct gpio_dt_spec led_dev = GPIO_DT_SPEC_GET(DT_ALIAS(led2), gpios);
+static const struct gpio_dt_spec led_dev = GPIO_DT_SPEC_GET(DT_ALIAS(led2), gpios);
 static const struct device *const dmic_dev = DEVICE_DT_GET(DT_NODELABEL(mp34dt06j));
 
 static struct pcm_stream_cfg stream = {
@@ -52,7 +56,7 @@ static struct dmic_cfg cfg = {
 	},
 };
 
-
+static volatile int16_t audio_buf[AUDIO_BUF_SIZE];
 
 int main(void)
 {
@@ -77,41 +81,42 @@ int main(void)
 		return 0;
 	}
 
-	for (;;)
+	k_msleep(500);
+
+	ret = dmic_trigger(dmic_dev, DMIC_TRIGGER_START);
+
+	if (ret < 0)
 	{
-		ret = dmic_trigger(dmic_dev, DMIC_TRIGGER_START);
+		LOG_ERR("DMIC trigger error %d", ret);
+		return 0;
+	}
+
+	size_t sample_count;
+
+	for (sample_count = 0; sample_count < AUDIO_BUF_SIZE;)
+	{
+		void *pcm_buffer;
+		size_t size; 
+
+		ret = dmic_read(dmic_dev, 0, &pcm_buffer, &size, READ_TIMEOUT_MS);
 
 		if (ret < 0)
 		{
-			LOG_ERR("DMIC trigger error %d", ret);
+			LOG_ERR("DMIC read failed with err %d", ret);
 			return 0;
 		}
 
-		for (int i = 0; i < 2*BLOCK_COUNT; i++)
-		{
-			void *buffer;
-			uint32_t size;
+		memcpy((void *) &audio_buf[sample_count], pcm_buffer, size);
 
-			ret = dmic_read(dmic_dev, 0, &buffer, &size, READ_TIMEOUT_MS);
+		sample_count += size / sizeof(int16_t);
 
-			if (ret < 0)
-			{
-				LOG_ERR("%d - read failed with err %d", i, ret);
-				return 0;
-			}
+		k_mem_slab_free(&mem_slab, pcm_buffer);
+	}
 
-			LOG_INF("%d - got buffer %p of %u bytes", i, buffer, size);
-
-			k_mem_slab_free(&mem_slab, buffer);
-		}
-
-		ret = dmic_trigger(dmic_dev, DMIC_TRIGGER_STOP);
-		if (ret < 0) {
-			LOG_ERR("STOP trigger failed: %d", ret);
-			return 0;
-		}
-
-		k_msleep(1000);
+	ret = dmic_trigger(dmic_dev, DMIC_TRIGGER_STOP);
+	if (ret < 0) {
+		LOG_ERR("STOP trigger failed: %d", ret);
+		return 0;
 	}
 
 	return 0;
