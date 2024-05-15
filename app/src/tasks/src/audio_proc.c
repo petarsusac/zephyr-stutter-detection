ff819ac360bfa95bbacbf1317cd690871716cdd6
Petar Susac <petar.susac@fer.hr>
2024-05-15 15:51:21 +0200

    Use internal RTC for timestamping

--- app/src/tasks/src/audio_proc.c
+++ app/src/tasks/src/audio_proc.c
@@ -2,6 +2,9 @@
 
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/rtc.h>
+#include <zephyr/sys/timeutil.h>
 #include <stdio.h>
 
 #include "inference.h"
@@ -10,17 +13,52 @@
 #include "audio_acq.h"
 
 #define FEATURE_SIZE (13*43)
+#define FILENAME_LEN (18U)
+#define SET_TIME 1
 
 LOG_MODULE_REGISTER(audio_proc, LOG_LEVEL_DBG);
 
-static const char* p_filename = "20240502-1134.txt";
-static char p_line[STORAGE_MAX_LINE_LEN];
+static const struct device *const p_rtc_dev = DEVICE_DT_GET(DT_NODELABEL(rtc));
+
+static char filename[FILENAME_LEN];
+static char line[STORAGE_MAX_LINE_LEN];
 
 static K_SEM_DEFINE(proc_start_sem, 0, 1);
 
 int audio_proc_init(void)
 {
 	int ret;
+	struct rtc_time rtc_ts;
+
+#if SET_TIME
+	rtc_ts.tm_year = 124;
+	rtc_ts.tm_mon = 5;
+	rtc_ts.tm_mday = 14;
+	rtc_ts.tm_wday = 2;
+	rtc_ts.tm_hour = 10;
+	rtc_ts.tm_min = 24;
+	rtc_ts.tm_sec = 30;
+	rtc_ts.tm_nsec = 0;
+
+	ret = rtc_set_time(p_rtc_dev, &rtc_ts);
+	if (ret != 0)
+	{
+		return ret;
+	}
+#endif /* SET_TIME */
+
+	ret = rtc_get_time(p_rtc_dev, &rtc_ts);
+	if (ret != 0)
+	{
+		return ret;
+	}
+
+	sprintf(filename, "%04d%02d%02d-%02d%02d.csv",
+		rtc_ts.tm_year + 1900,
+		rtc_ts.tm_mon,
+		rtc_ts.tm_mday,
+		rtc_ts.tm_hour,
+		rtc_ts.tm_min);
 
 	ret = storage_init();
 	// Temporarily commented out to be able to debug without using the SD card
@@ -54,6 +92,8 @@ void audio_proc_run(void *p1, void *p2, void *p3)
 	int ret;
 	float mfcc[FEATURE_SIZE];
 	output_values_t output;
+	struct rtc_time rtc_ts;
+	int64_t posix_ts;
 
 	int16_t *p_audio_buf = audio_acq_get_secondary_buf_ptr();
 
@@ -80,13 +120,16 @@ void audio_proc_run(void *p1, void *p2, void *p3)
 
 			LOG_DBG("Elapsed: %lld ms", diff);
 
-			sprintf(p_line, 
-					"yyyy-mm-dd-hh-mm-ss,%d,%d,%d\n",
+			rtc_get_time(p_rtc_dev, &rtc_ts);
+			posix_ts = timeutil_timegm64(rtc_time_to_tm(&rtc_ts));
+
+			sprintf(line, "%lld,%d,%d,%d\n",
+					posix_ts,
 					(int) (output.block * 100),
 					(int) (output.prolongation * 100),
 					(int) (output.repetition * 100));
 
-			storage_write_line(p_line, p_filename);
+			storage_write_line(line, filename);
 		}
 	}
 }
