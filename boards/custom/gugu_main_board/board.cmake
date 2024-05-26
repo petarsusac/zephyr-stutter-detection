c7940854e23d24a31a3f52ebb508dfc144eb25e8
Petar Susac <petar.susac@fer.hr>
2024-05-26 12:09:23 +0200

    Change SoC to F746VG and disable HSE bypass

--- boards/custom/gugu_main_board/board.cmake
+++ boards/custom/gugu_main_board/board.cmake
@@ -1,3 +1,3 @@
-board_runner_args(jlink "--device=STM32F746ZG" "--speed=4000" "--reset")
+board_runner_args(jlink "--device=STM32F746VG" "--speed=4000" "--reset")
 
 include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
